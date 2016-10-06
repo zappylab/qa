f206f602e0fea42de55db99dd3d55a152c2d649a
vasily <vasily@zappylab.com>
2016-10-06 13:05:41 +0300

    separate rake tasks for sign in and groups

--- spec/Rakefile.rb
+++ spec/Rakefile.rb
@@ -1,8 +1,15 @@
 require 'rspec/core/rake_task'
 
 RSpec::Core::RakeTask.new(:spec) do |t|
-  t.pattern = "./test_spec.rb"
-  t.rspec_opts = "--format RspecJunitFormatter --out results.xml --format html --out results.html"
+  t.pattern = "./sign_in_spec.rb"
+  t.rspec_opts = "--format RspecJunitFormatter --out sign_in_spec.xml --format html --out sign_in_spec.html"
 end
 
-task :default => :spec
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.pattern = "./groups_workflow_spec.rb"
+  t.rspec_opts = "--format RspecJunitFormatter --out groups_workflow_spec.xml --format html --out groups_workflow_spec.html"
+end
+
+#This is list of rake tasks for test suites
+task :sign_in => :spec 	# this task describe sign in, sign up and delete account
+task :groups => :spec 	# this task describe group workflow: create, delete groups, create groups with invitations and confirm invite
