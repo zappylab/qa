98e0b8819b16c3396d8a038571dcce3de44b894e
vasily <vasily@zappylab.com>
2016-10-10 12:32:53 +0300

    modified user_profile_lib delete_user

--- spec/lib/user_profile_lib.rb
+++ spec/lib/user_profile_lib.rb
@@ -12,12 +12,7 @@ module UserProfilePageModule
 		def delete_user
 			find(:xpath, ".//div[@class='main-action-btn']").hover
 			find_all(:xpath, ".//div[@class='action-btns']")[0].click
-			# page.execute_script "document.getElementsByClassName('lighbox-Settings')[0].scrollBy(0,10000)"
-			script = "arguments[0].scrollIntoView(true);"
-			element = page.all(:xpath, ".//a[@class='deleteaccount']", :all)[0]
-			Capybara.current_session.driver.browser.execute_script(script, element.native)
-			element.click
-			# find(:css, ".deleteaccount").click
+			find(:xpath, ".//a[@class='deleteaccount']").click
 			find("#profile-delete-btn").click
 			page.has_selector?("#app")
     	end
