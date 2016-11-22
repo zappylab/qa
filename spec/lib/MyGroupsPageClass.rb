64517b393a1505b69340c73e439d4fd87995afee
vasily <vasily@zappylab.com>
2016-11-22 12:47:24 +0300

    groups search

--- spec/lib/MyGroupsPageClass.rb
+++ spec/lib/MyGroupsPageClass.rb
@@ -7,16 +7,16 @@ class MyGroupsPageClass
 
 	def search_group(group_name)
 		find(:xpath, ".//*[@id='left_side']//*/li[text()='Joined']").click
-		find("#header-search-input").click
-        find("#header-search-input").set(group_name)
-        find("#header-search-input").native.send_keys(:enter)
+		find(:xpath, ".//input[@id='header-search-input']", visible: true).click
+        find(:xpath, ".//input[@id='header-search-input']", visible: true).set(group_name)
+        find(:xpath, ".//input[@id='header-search-input']", visible: true).native.send_keys(:enter)
         puts '		LOG: Searching group ' + group_name
-        find(:xpath, ".//ul[@class='community-search-groups']//*/a[@class='gi-name' and text()='" + group_name + "']")
+        find(:xpath, ".//ul//*/a[@class='gi-name' and text()='" + group_name + "']")
         puts '		LOG: Group ' + group_name + ' found'
     end
 
 	def drill_down_group
-		find(:xpath, ".//ul[@class='community-search-groups']/li").click
+		find(:xpath, ".//li[contains(@class, 'gi-item')]").click
 		page.has_selector?(:xpath, ".//div[@class='community-logo']")
 		return ProtocolsInGroupPageClass.new
 	end 
