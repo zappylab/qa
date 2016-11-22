9684885fc3e24a708a19a86c8bd5104840844331
vasily <vasily@zappylab.com>
2016-11-22 13:22:28 +0300

    search in header

--- spec/lib/MyGroupsPageClass.rb
+++ spec/lib/MyGroupsPageClass.rb
@@ -9,11 +9,19 @@ class MyGroupsPageClass
         find(:xpath, ".//*[@id='left_side']//*/li[text()='Created']").click
 		find(:xpath, ".//input[@id='header-search-input']", visible: true).click
         find(:xpath, ".//input[@id='header-search-input']", visible: true).set(group_name)
-        find(:xpath, ".//input[@id='header-search-input']", visible: true).native.send_keys(:enter)
-        sleep 3.0
-        puts '		LOG: Searching group ' + group_name
-        find(:xpath, ".//ul//*/a[text()='" + group_name + "']")
-        puts '		LOG: Group ' + group_name + ' found'
+        # find(:xpath, ".//input[@id='header-search-input']", visible: true).native.send_keys(:enter)
+        find(:xpath, ".//div[@class='header-search-input']//*/i[@class='p-font pf-search']").click
+
+        begin 
+ 			find(:xpath, ".//ul//*/a[text()='" + group_name + "']", wait: 5)
+	        puts '		LOG: Searching group ' + group_name
+	        puts '		LOG: Group ' + group_name + ' found'
+        rescue Capybara::ElementNotFond
+        	puts '		LOG: searching group again... '
+        	find(:xpath, ".//div[@class='header-search-input']//*/i[@class='p-font pf-search']").click
+        	find(:xpath, ".//ul//*/a[text()='" + group_name + "']", wait: 5)
+        	puts '		LOG: Group ' + group_name + ' found'
+        end
     end
 
 	def drill_down_group
