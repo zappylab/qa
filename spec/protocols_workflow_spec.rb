1ae4a821be8b1b86e1b1ab15cb37a7fa7c16f8ca
vasily <vasily@zappylab.com>
2016-12-22 15:52:58 +0300

    remove unlisted test

--- spec/protocols_workflow_spec.rb
+++ spec/protocols_workflow_spec.rb
@@ -102,39 +102,39 @@ describe 'Working with protocols' do
 		myProtocolsPage.sign_out
 	end
 
-	it 'should create new protocol and publish to unlisted' do
-		loginPage = LoginPageClass.new
-		startPage = loginPage.sign_in "vasily@zappylab.com", "NLg6v5JT"
-		myProtocolsPage = startPage.go_to_my_protocols
-		editPage = myProtocolsPage.start_new_protocol
-		created_protocol = "test" + (Time.now.nsec).to_s
-		editPage.set_protocol_name created_protocol
-		editPage.save_protocol
-		for i in 0..2 do
-			editPage.add_step
-		end
-		editPage.focus_name
-		editPage.save_protocol
-		for i in 1..4 do
-			editPage.focus_name
-			editPage.focus_step i
-			editPage.set_desc_to_step (Time.now.nsec).to_s
-		end
-	    editPage.save_protocol
-	    editPage.go_to_my_protocols
-	    myProtocolsPage.select_explorer_item_by_name "My private"
-	    myProtocolsPage.focus_protocol_by_name created_protocol
-	    myProtocolsPage.publish_protocol("unlisted")
-	    myProtocolsPage.select_explorer_item_by_name "My unlisted"
-	    begin
-	    	puts "		LOG: Start searching created protocol with name = " + created_protocol + " in unlisted"
-	    	myProtocolsPage.focus_protocol_by_name created_protocol
-	    	puts "		LOG: Start searching created protocol with name = " + created_protocol + " found in unlisted"
-		rescue Capybara::ElementNotFound
-			puts "		LOG: Created protocol with name = " + created_protocol + " wasn't published"
-		end
-		myProtocolsPage.sign_out
-	end
+	# it 'should create new protocol and publish to unlisted' do
+	# 	loginPage = LoginPageClass.new
+	# 	startPage = loginPage.sign_in "vasily@zappylab.com", "NLg6v5JT"
+	# 	myProtocolsPage = startPage.go_to_my_protocols
+	# 	editPage = myProtocolsPage.start_new_protocol
+	# 	created_protocol = "test" + (Time.now.nsec).to_s
+	# 	editPage.set_protocol_name created_protocol
+	# 	editPage.save_protocol
+	# 	for i in 0..2 do
+	# 		editPage.add_step
+	# 	end
+	# 	editPage.focus_name
+	# 	editPage.save_protocol
+	# 	for i in 1..4 do
+	# 		editPage.focus_name
+	# 		editPage.focus_step i
+	# 		editPage.set_desc_to_step (Time.now.nsec).to_s
+	# 	end
+	#     editPage.save_protocol
+	#     editPage.go_to_my_protocols
+	#     myProtocolsPage.select_explorer_item_by_name "My private"
+	#     myProtocolsPage.focus_protocol_by_name created_protocol
+	#     myProtocolsPage.publish_protocol("unlisted")
+	#     myProtocolsPage.select_explorer_item_by_name "My unlisted"
+	#     begin
+	#     	puts "		LOG: Start searching created protocol with name = " + created_protocol + " in unlisted"
+	#     	myProtocolsPage.focus_protocol_by_name created_protocol
+	#     	puts "		LOG: Start searching created protocol with name = " + created_protocol + " found in unlisted"
+	# 	rescue Capybara::ElementNotFound
+	# 		puts "		LOG: Created protocol with name = " + created_protocol + " wasn't published"
+	# 	end
+	# 	myProtocolsPage.sign_out
+	# end
 
 	it 'should create several version of protocol' do
 		loginPage = LoginPageClass.new
